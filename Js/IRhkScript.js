f60859342aeb3b1111f9ecb9cbb51e266c391566
suchit1986 <suchit.gollu@gmail.com>
2015-12-24 16:16:21 +0530

    Updated with encyption and notifications

---
+++ Js/IRhkScript.js
@@ -0,0 +1,15 @@
+﻿var Handler = "ALSHandler.ashx";
+function GetIRRemoteInfo(callback) {
+    $.ajax({
+        type: 'GET',
+        url: Handler,
+        dataType: 'json',
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+        }
+
+    });
+
+}
---
+++ Js/hkScript.js
@@ -0,0 +1,203 @@
+﻿var Handler = "";
+var authAjaxReqTimeOut = 2000;
+var ajaxReqTimeOut = 60000;
+function ALSUserAuthenticationRequest(queryString, param, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: param,
+        type: "POST",
+        timeout: authAjaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback("true");
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback("false");
+        }
+
+    });
+};
+
+function GetUUIDConfig(queryString, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: {},
+        type: "GET",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+function GetIRConfig(queryString, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: {},
+        type: "GET",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+
+function GetGlobalScene(queryString, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: {},
+        type: "GET",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+
+function GetNotificationConfig(queryString, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: {},
+        type: "GET",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+
+function SetGlobalScene(queryString, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: {},
+        type: "GET",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+
+
+function ALSReadAjaxRequest(queryString, param, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: param,
+        type: "POST",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+
+function ALSWriteAjaxRequest(queryString, param, callback) {
+    Handler = GetNewHandler();
+    $.ajax({
+        url: Handler + queryString,
+        data: param,
+        type: "POST",
+        timeout: ajaxReqTimeOut,
+        beforeSend: function (xhr) {
+
+        },
+        complete: function () {
+
+        },
+        success: function (data) {
+            callback(data);
+        },
+        error: function (data) {
+            $.mobile.loading("hide");
+            try { var elMask = document.getElementById('loaddiv'); elMask.parentNode.removeChild(elMask); } catch (msg) { }
+            callback(data);
+        }
+
+    });
+};
+
